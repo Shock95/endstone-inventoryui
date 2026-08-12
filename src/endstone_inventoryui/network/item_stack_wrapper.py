2e2d45e9f0f53910d69803423c6f9f611f9e3b1c
Shock95 <16858059+Shock95@users.noreply.github.com>
2026-08-11 21:38:11 -0400

    refactor(item_stack_wrapper.py): remove unused code

--- src/endstone_inventoryui/network/item_stack_wrapper.py
+++ src/endstone_inventoryui/network/item_stack_wrapper.py
@@ -7,29 +7,18 @@ from endstone_inventoryui.util import item_utils
 class ItemStackWrapper:
     stack_id: int
     item_stack: ItemStack
-    stack_id_variant: int
     data: ItemData
 
-    def __init__(self, stack_id: int = 0, item_stack: ItemStack | None = None, stack_id_variant: int = 0):
+    def __init__(self, stack_id: int = 0, item_stack: ItemStack | None = None):
         from endstone_inventoryui.util.item_utils import get_item_data
         self.stack_id: int = stack_id
-        self.stack_id_variant: int = stack_id_variant
         self.item_stack: ItemStack = item_stack or ItemStack("minecraft:air")
         data = get_item_data(self.item_stack.type.id)
         if data is None:
             raise ValueError(f"ItemStackWrapper: ItemData not found for {self.item_stack.type.id}")
         self.data: ItemData = data
 
-    def write_header(self, stream: BinaryStream) -> bool:
-        if item_utils.is_air(self.item_stack):
-            stream.write_varint(0)
-            return False
-        stream.write_varint(self.data.item_id)
-        stream.write_unsigned_short(self.item_stack.amount)
-        stream.write_unsigned_varint(self.item_stack.data)
-        return True
-
-    def write_footer(self, stream: BinaryStream):
+    def write_extra_data(self, stream: BinaryStream):
         """Builds the extra data buffer (NBT + canPlaceOn + canDestroy)"""
         item_meta = self.item_stack.item_meta
         tag = item_utils.build_tag(item_meta)
@@ -61,27 +50,5 @@ class ItemStackWrapper:
             return
 
         user_data = BinaryStream()
-        self.write_footer(user_data)
-        stream.write_bytes(user_data.copy_buffer())  # user data
-
-    def write_descriptor(self, stream: BinaryStream):
-        is_air = item_utils.is_air(self.item_stack)
-        has_net_id = self.stack_id != 0
-
-        stream.write_signed_short(0 if is_air else self.data.item_id)
-        stream.write_unsigned_short(self.item_stack.amount)
-        stream.write_unsigned_varint(self.item_stack.data)
-
-        stream.write_bool(has_net_id)
-        if has_net_id:
-            stream.write_unsigned_varint(0)
-            stream.write_varint(self.stack_id)
-
-        stream.write_unsigned_varint(0)  # BlockRuntimeID
-        if is_air:
-            stream.write_unsigned_varint(0)
-            return
-
-        user_data = BinaryStream()
-        self.write_footer(user_data)
-        stream.write_bytes(user_data.copy_buffer())  # user data
+        self.write_extra_data(user_data)
+        stream.write_bytes(user_data.get_and_release_data())
